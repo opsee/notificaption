af8878bc11900c4bf6330af75ffee84167ab34b3
Sara Bee <sara.bainbridge@gmail.com>
2016-01-04 17:01:58 -0800

    Generate screenshot on each POST request

--- notificaption.js
+++ notificaption.js
@@ -1,6 +1,22 @@
+var Nightmare = require('nightmare');
 var restify = require('restify');
 
+const nightmare = Nightmare({
+  width: 350
+});
+
+function generateScreenshot() {
+  return nightmare
+    .goto('http://localhost:8080')
+    .screenshot();
+}
+
 function postScreenshot(req, res, next) {
+  generateScreenshot()
+    .then(buffer => {
+      console.log('all done');
+    });
+
   res.send(req.body);
   next();
 }
